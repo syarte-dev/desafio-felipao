ba68036e231bedd5041aced8761a1c948f8e42a0
Laís Braz <syarte.dev@gmail.com>
2024-09-18 21:08:28 -0300

    corrigido erros

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 //Nivel do heroi desafio DIO Felipao
+// Corrigido erro em XP do heroi para número inteiro
 const nomeHeroi = "Nina Felina"
-const xpDoHeroi = "5849"
+const xpDoHeroi = 150000;
 
+//Corrigido erros com ";"
 let nivelDoHeroi;
 if (xpDoHeroi < 1000) {
-    nivelDoHeroi = "Ferro;"
+    nivelDoHeroi = "Ferro";
 } else if (xpDoHeroi >= 1001 && xpDoHeroi <=2000
 ) {
-    nivelDoHeroi = "Bronze;"
+    nivelDoHeroi = "Bronze";
 } else if (xpDoHeroi >= 2001 && xpDoHeroi <=5000
 ) {
-    nivelDoHeroi = "Prata;"
+    nivelDoHeroi = "Prata";
 } else if (xpDoHeroi >= 5001 && xpDoHeroi <=7000
 ) {
-    nivelDoHeroi = "Ouro;"
+    nivelDoHeroi = "Ouro";
 } else if (xpDoHeroi >= 7001 && xpDoHeroi <=8000
 ) {
-    nivelDoHeroi = "Platina;"
+    nivelDoHeroi = "Platina";
 } else if (xpDoHeroi >= 8001 && xpDoHeroi <=9000
 ) {
-    nivelDoHeroi = "Ascendente;"
+    nivelDoHeroi = "Ascendente";
 } else if (xpDoHeroi >= 9001 && xpDoHeroi <=10000
 ) {
-    nivelDoHeroi = "Imortal;"
+    nivelDoHeroi = "Imortal";
 } else if (xpDoHeroi >= 10001 && xpDoHeroi
 ) { nivelDoHeroi = "Radiante";
 }
-console.log('O Herói de nome ${nomeHeroi} está no nível de ${nivelDoHeroi}.');
-
-
-
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelDoHeroi}.`);
 
 
